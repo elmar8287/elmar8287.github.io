15293e3a52a6e9654b1216b462adfe8696395cf4
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-06-28 17:42:26 +0300

    Add scrollspy to Nav menu

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
-//   getElementById("myNav").style.filter = "blur(10px)";
 }
   
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+
 
