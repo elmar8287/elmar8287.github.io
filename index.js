c0fbed77f21caef4b769e272011884969c761ef3
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-02 14:52:56 +0300

    Complete saving data to local storage code

--- index.js
+++ index.js
@@ -215,3 +215,39 @@ function saveFormDataToLocalStorage(name, email, message) {
   };
   localStorage.setItem('contactInfo', JSON.stringify(contactInfo));
 }
+const nameInput = document.querySelector('footer form input[type="text"]');
+const emailInput = document.querySelector('footer form input[type=email]');
+const messageInput = document.querySelector('footer form textarea');
+const inputsArray = [emailInput, nameInput, messageInput];
+// GETTING FORM DATA FROM LOCAL STORAGE
+window.addEventListener('load', () => {
+  if (JSON.parse(localStorage.getItem('contactInfo'))) {
+    const { name, email, message } = JSON.parse(
+      localStorage.getItem('contactInfo')
+    );
+    (emailInput.value = email),
+      (nameInput.value = name),
+      (messageInput.value = message);
+  }
+});
+inputsArray.forEach((input) =>
+  input.addEventListener('input', (e) => {
+    if (input === nameInput) {
+      nameInput.value = input.value;
+    } else if (input === emailInput) {
+      emailInput.value = input.value;
+      // VALIDATION EMAIL INPUT
+      if (e.target.value !== emailInput.value.toLowerCase()) {
+        e.preventDefault();
+        emailInput.parentElement.classList.add('invalidInput');
+      } else {
+        saveFormDataToLocalStorage(nameInput, emailInput, messageInput);
+        emailInput.parentElement.classList.remove('invalidInput');
+      }
+    } else {
+      messageInput.value = input.value;
+    }
+    saveFormDataToLocalStorage(nameInput, emailInput, messageInput);
+  })
+);
+
