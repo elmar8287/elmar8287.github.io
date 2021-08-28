c861daa6167719e740485188a324a5d9de8914a2
elmar8287 <L-mar@inbox.ru>
2021-08-28 18:05:22 +0400

    Fic mobile menu

--- test.js
+++ test.js
@@ -1,40 +1,3 @@
-const mobileMenu = document.createElement('div');
-mobileMenu.className = 'mobileMenu';
-document.body.appendChild(mobileMenu);
-const closeIcon = document.createElement('i');
-closeIcon.className = 'fas fa-times';
-mobileMenu.appendChild(closeIcon);
-const menuItems = document.createElement('ul');
-menuItems.className = 'menuItems';
-menuItems.innerHTML = `<li><a href="#portfolio">Portfolio</a></li>
-                      <li><a href="#about">About</a></li>
-                      <li><a href="#contact">Contact</a></li>`;
-mobileMenu.appendChild(menuItems);
-const blurred = document.querySelectorAll('.filter');
-document.getElementById('mobile-menu').addEventListener('click', () => {
-  mobileMenu.style.display = 'flex';
-  blurred.forEach((section) => {
-    section.style.filter = 'blur(5px)';
-  });
-});
-
-closeIcon.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
-  blurred.forEach((section) => {
-    section.style.filter = 'blur(0px)';
-  });
-});
-
-const menuLinks = menuItems.querySelectorAll('a');
-menuLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    mobileMenu.style.display = 'none';
-    blurred.forEach((section) => {
-      section.style.filter = 'blur(0px)';
-    });
-  });
-});
-
 const projects = [
   {
     id: 0,
@@ -234,3 +197,15 @@ form.addEventListener('input', (evt) => {
 resetBtn.addEventListener('click', () => {
   localStorage.setItem('userInfo', userData);
 });
+
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+openNav();
+
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+closeNav();
