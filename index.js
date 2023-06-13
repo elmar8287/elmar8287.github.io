7644b0c2a49b5ca97175e8d1436a12fa52cf1e38
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2023-06-13 21:36:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -48,184 +48,184 @@ const projects = [
     liveSource: 'https://dreamcarsautosales.com/',
     sourceCode: 'https://github.com/elmar8287/dreamcar-auto-sales',
     }
-   // ,
-  // {
-  //   id: 3,
-  //   name: 'Covid-19 Metrics',
-  //   companyName: 'Covid-19',
-  //   position: 'Full stack',
-  //   year: 2021,
-  //   description:
-  //   'The project is about building a mobile web application to check a list of Worldwide COVID-19 metrics (numeric values). By clicking on the country name, there is a possibility to view more detailed information.',
-  //   technologies: ['css', 'react', 'redux', 'javascript', 'netlify'],
-  //   image: {
-  //     link: './img/project8.PNG',
-  //     alt: 'COVID-19 Metrics',
-  //   },
-  //   liveSource: 'https://elmar-project-covid19.netlify.app/',
-  //   sourceCode: 'https://github.com/elmar8287/metrics',
-  // },
-  //   {
-  //   id: 4,
-  //   name: 'MLH Prep React Weather APP',
-  //   companyName: 'MLH - Major League Hacking',
-  //   position: 'Front End Dev',
-  //   year: 2022,
-  //   description:
-  //     'This is a React Application, which fetches data from Open Weather API & Open AI API then displays it on UI.The displayed data is related to the weather conditions, activities available, sunrise-sunset times, suggestions, AQI & daily forecasts of the selected city (or coordinates)',
-  //   technologies: ['react', 'docker', 'api', 'netlify'],
-  //   image: {
-  //     link: './img/project11.png',
-  //     alt: 'MLH Prep React Weather App',
-  //   },
-  //   liveSource: 'https://prep-22-jul-prep-3-project.netlify.app/',
-  //   sourceCode: 'https://github.com/MLH-Fellowship/prep-project-22.JUL.PREP.3',
-  // },
+   ,
+  {
+    id: 3,
+    name: 'Covid-19 Metrics',
+    companyName: 'Covid-19',
+    position: 'Full stack',
+    year: 2021,
+    description:
+    'The project is about building a mobile web application to check a list of Worldwide COVID-19 metrics (numeric values). By clicking on the country name, there is a possibility to view more detailed information.',
+    technologies: ['css', 'react', 'redux', 'javascript', 'netlify'],
+    image: {
+      link: './img/project8.PNG',
+      alt: 'COVID-19 Metrics',
+    },
+    liveSource: 'https://elmar-project-covid19.netlify.app/',
+    sourceCode: 'https://github.com/elmar8287/metrics',
+  },
+    {
+    id: 4,
+    name: 'MLH Prep React Weather APP',
+    companyName: 'MLH - Major League Hacking',
+    position: 'Front End Dev',
+    year: 2022,
+    description:
+      'This is a React Application, which fetches data from Open Weather API & Open AI API then displays it on UI.The displayed data is related to the weather conditions, activities available, sunrise-sunset times, suggestions, AQI & daily forecasts of the selected city (or coordinates)',
+    technologies: ['react', 'docker', 'api', 'netlify'],
+    image: {
+      link: './img/project11.png',
+      alt: 'MLH Prep React Weather App',
+    },
+    liveSource: 'https://prep-22-jul-prep-3-project.netlify.app/',
+    sourceCode: 'https://github.com/MLH-Fellowship/prep-project-22.JUL.PREP.3',
+  },
 
-  // {
-  //   id: 5,
-  //   name: 'Digitalization in Medicine',
-  //   companyName: 'AIMedicine',
-  //   position: 'Front End Dev',
-  //   year: 2021,
-  //   description:
-  //     'This is a forum website with all necessary information about upcoming events, speakers, and sponsors. You can add program details and a registration form. In this template, you do not need to signup, but you can add this functionality in the future. Please, consider, that the whole content on this website is fictional.',
-  //   technologies: ['html', 'css', 'javascript'],
-  //   image: {
-  //     link: './img/pproject0.jpg',
-  //     alt: 'Digitalization in Medicine',
-  //   },
-  //   liveSource: 'https://elmar8287.github.io/capstone/',
-  //   sourceCode: 'https://github.com/elmar8287/capstone',
-  // },
-  // {
-  //   id: 6,
-  //   name: 'TODO list in React',
-  //   companyName: 'Company X',
-  //   position: 'Front End Dev',
-  //   year: 2021,
-  //   description:
-  //     'This is a SPA todo-list, helps to organize tasks. You can easily add, delete and edit todos. Also, there is the possibility to mark tasks as completed. Local storage will keep your task on the list after refreshing and either close/open the browser.',
-  //   technologies: ['react', 'css'],
-  //   image: {
-  //     link: './img/project4.png',
-  //     alt: 'TODO list',
-  //   },
-  //   liveSource: 'https://elmar8287.github.io/react-tuto/',
-  //   sourceCode: 'https://github.com/elmar8287/react-tuto',
-  // },
-  // {
-  //   id: 7,
-  //   name: 'Awesome Books',
-  //   companyName: 'Company Y',
-  //   position: 'Front End Dev',
-  //   year: 2021,
-  //   description:
-  //     'Nice designed project with Bootstrap. Used single-page application. This is a project that displays a list of books and allows you to add and remove books from that list. Each book is related to the author. You can fix the number of books, which will be displayed. In this template, you can see the max number of books per page 6.',
-  //   technologies: ['html', 'luxon', 'css', 'bootstrap', 'javascript'],
-  //   image: {
-  //     link: './img/project1.png',
-  //     alt: 'Awesome Books screenshot',
-  //   },
-  //   liveSource: 'https://elmar8287.github.io/awesome-books/',
-  //   sourceCode: 'https://github.com/elmar8287/awesome-books',
-  // },
-  // {
-  //   id: 8,
-  //   name: 'JS API project - Meals DB',
-  //   companyName: 'Company Z',
-  //   position: 'Front End Dev',
-  //   year: 2021,
-  //   description:
-  //     'Meal API allows us to get a list of items with a unique item id (or generate the unique id), and for a given item, get detailed information about it. Also used other API, where saved likes and comments for each item.',
-  //   technologies: ['html', 'css', 'bootstrap', 'javascript'],
-  //   image: {
-  //     link: './img/project3.png',
-  //     alt: 'JS API project',
-  //   },
-  //   liveSource: 'https://aliabbani.github.io/Lemonade-api/dist/',
-  //   sourceCode: 'https://github.com/aliabbani/Lemonade-api',
-  // },
-  // {
-  //   id: 9,
-  //   name: 'Space Travelers Hub',
-  //   companyName: 'SpaceX',
-  //   position: 'Front End Dev',
-  //   year: 2021,
-  //   description:
-  //     'A web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
-  //   technologies: ['css', 'react', 'redux', 'javascript', 'netlify'],
-  //   image: {
-  //     link: './img/project7.PNG',
-  //     alt: 'Space Travelers Hub',
-  //   },
-  //   liveSource: 'https://space-travelers.netlify.app/',
-  //   sourceCode: 'https://github.com/elmar8287/space-travelers',
-  // },
-  // {
-  //   id: 10,
-  //   name: 'Leaderboard API',
-  //   companyName: 'Company B',
-  //   position: 'Front End Dev',
-  //   year: 2021,
-  //   description:
-  //     'The leaderboard website displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API service.',
-  //   technologies: ['html', 'css', 'bootstrap', 'javascript'],
-  //   image: {
-  //     link: './img/project2.png',
-  //     alt: 'TodoList',
-  //   },
-  //   liveSource: 'https://elmar8287.github.io/leaderboard/',
-  //   sourceCode: 'https://github.com/elmar8287/leaderboard',
-  // },
-  // {
-  //   id: 11,
-  //   name: 'Math Magicians',
-  //   companyName: 'Company C',
-  //   position: 'Full stack',
-  //   year: 2021,
-  //   description:
-  //     'It is a Single Page App (SPA) built in React. This project allows users to make simple calculations and read a random math-related quote.',
-  //   technologies: ['css', 'react', 'javascript', 'heroku', 'netlify'],
-  //   image: {
-  //     link: './img/project5.PNG',
-  //     alt: 'Math Magicians',
-  //   },
-  //   liveSource: 'https://elmar8287.github.io/react-math/',
-  //   sourceCode: 'https://github.com/elmar8287/react-math',
-  // },
-  // {
-  //   id: 12,
-  //   name: 'Bookstore',
-  //   companyName: 'Company D',
-  //   position: 'Full stack',
-  //   year: 2021,
-  //   description:
-  //     'This is a React application related to add, delete and display favorite books. Here you can see progress in reading for each book.',
-  //   technologies: ['css', 'react', 'redux', 'javascript', 'netlify'],
-  //   image: {
-  //     link: './img/project6.PNG',
-  //     alt: 'Bookstore',
-  //   },
-  //   liveSource: 'https://bookstore-abdulkae.netlify.app/',
-  //   sourceCode: 'https://github.com/elmar8287/Bookstore',
-  // },
-  // {
-  //   id: 13,
-  //   name: 'MLH Prep Portfolio Web-page',
-  //   companyName: 'MLH - Major League Hacking',
-  //   position: 'Front End Dev',
-  //   year: 2022,
-  //   description:
-  //     'The project is a Pod portfolio web-page, while being a part of MLH Prep July 2022 three weeks program.',
-  //   technologies: ['jekyll', 'js', 'html', 'css', 'netlify'],
-  //   image: {
-  //     link: './img/project10.png',
-  //     alt: 'MLH Pod Portfolio Web-page',
-  //   },
-  //   liveSource: 'https://prep-22-jul-prep-3-portfolio.netlify.app/',
-  //   sourceCode: 'https://github.com/MLH-Fellowship/prep-portfolio-22.JUL.PREP.3',
-  // },
+  {
+    id: 5,
+    name: 'Digitalization in Medicine',
+    companyName: 'AIMedicine',
+    position: 'Front End Dev',
+    year: 2021,
+    description:
+      'This is a forum website with all necessary information about upcoming events, speakers, and sponsors. You can add program details and a registration form. In this template, you do not need to signup, but you can add this functionality in the future. Please, consider, that the whole content on this website is fictional.',
+    technologies: ['html', 'css', 'javascript'],
+    image: {
+      link: './img/pproject0.jpg',
+      alt: 'Digitalization in Medicine',
+    },
+    liveSource: 'https://elmar8287.github.io/capstone/',
+    sourceCode: 'https://github.com/elmar8287/capstone',
+  },
+  {
+    id: 6,
+    name: 'TODO list in React',
+    companyName: 'Company X',
+    position: 'Front End Dev',
+    year: 2021,
+    description:
+      'This is a SPA todo-list, helps to organize tasks. You can easily add, delete and edit todos. Also, there is the possibility to mark tasks as completed. Local storage will keep your task on the list after refreshing and either close/open the browser.',
+    technologies: ['react', 'css'],
+    image: {
+      link: './img/project4.png',
+      alt: 'TODO list',
+    },
+    liveSource: 'https://elmar8287.github.io/react-tuto/',
+    sourceCode: 'https://github.com/elmar8287/react-tuto',
+  },
+  {
+    id: 7,
+    name: 'Awesome Books',
+    companyName: 'Company Y',
+    position: 'Front End Dev',
+    year: 2021,
+    description:
+      'Nice designed project with Bootstrap. Used single-page application. This is a project that displays a list of books and allows you to add and remove books from that list. Each book is related to the author. You can fix the number of books, which will be displayed. In this template, you can see the max number of books per page 6.',
+    technologies: ['html', 'luxon', 'css', 'bootstrap', 'javascript'],
+    image: {
+      link: './img/project1.png',
+      alt: 'Awesome Books screenshot',
+    },
+    liveSource: 'https://elmar8287.github.io/awesome-books/',
+    sourceCode: 'https://github.com/elmar8287/awesome-books',
+  },
+  {
+    id: 8,
+    name: 'JS API project - Meals DB',
+    companyName: 'Company Z',
+    position: 'Front End Dev',
+    year: 2021,
+    description:
+      'Meal API allows us to get a list of items with a unique item id (or generate the unique id), and for a given item, get detailed information about it. Also used other API, where saved likes and comments for each item.',
+    technologies: ['html', 'css', 'bootstrap', 'javascript'],
+    image: {
+      link: './img/project3.png',
+      alt: 'JS API project',
+    },
+    liveSource: 'https://aliabbani.github.io/Lemonade-api/dist/',
+    sourceCode: 'https://github.com/aliabbani/Lemonade-api',
+  },
+  {
+    id: 9,
+    name: 'Space Travelers Hub',
+    companyName: 'SpaceX',
+    position: 'Front End Dev',
+    year: 2021,
+    description:
+      'A web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
+    technologies: ['css', 'react', 'redux', 'javascript', 'netlify'],
+    image: {
+      link: './img/project7.PNG',
+      alt: 'Space Travelers Hub',
+    },
+    liveSource: 'https://space-travelers.netlify.app/',
+    sourceCode: 'https://github.com/elmar8287/space-travelers',
+  },
+  {
+    id: 10,
+    name: 'Leaderboard API',
+    companyName: 'Company B',
+    position: 'Front End Dev',
+    year: 2021,
+    description:
+      'The leaderboard website displays scores submitted by different players. It also allows you to submit your score. All data is preserved thanks to the external Leaderboard API service.',
+    technologies: ['html', 'css', 'bootstrap', 'javascript'],
+    image: {
+      link: './img/project2.png',
+      alt: 'TodoList',
+    },
+    liveSource: 'https://elmar8287.github.io/leaderboard/',
+    sourceCode: 'https://github.com/elmar8287/leaderboard',
+  },
+  {
+    id: 11,
+    name: 'Math Magicians',
+    companyName: 'Company C',
+    position: 'Full stack',
+    year: 2021,
+    description:
+      'It is a Single Page App (SPA) built in React. This project allows users to make simple calculations and read a random math-related quote.',
+    technologies: ['css', 'react', 'javascript', 'heroku', 'netlify'],
+    image: {
+      link: './img/project5.PNG',
+      alt: 'Math Magicians',
+    },
+    liveSource: 'https://elmar8287.github.io/react-math/',
+    sourceCode: 'https://github.com/elmar8287/react-math',
+  },
+  {
+    id: 12,
+    name: 'Bookstore',
+    companyName: 'Company D',
+    position: 'Full stack',
+    year: 2021,
+    description:
+      'This is a React application related to add, delete and display favorite books. Here you can see progress in reading for each book.',
+    technologies: ['css', 'react', 'redux', 'javascript', 'netlify'],
+    image: {
+      link: './img/project6.PNG',
+      alt: 'Bookstore',
+    },
+    liveSource: 'https://bookstore-abdulkae.netlify.app/',
+    sourceCode: 'https://github.com/elmar8287/Bookstore',
+  },
+  {
+    id: 13,
+    name: 'MLH Prep Portfolio Web-page',
+    companyName: 'MLH - Major League Hacking',
+    position: 'Front End Dev',
+    year: 2022,
+    description:
+      'The project is a Pod portfolio web-page, while being a part of MLH Prep July 2022 three weeks program.',
+    technologies: ['jekyll', 'js', 'html', 'css', 'netlify'],
+    image: {
+      link: './img/project10.png',
+      alt: 'MLH Pod Portfolio Web-page',
+    },
+    liveSource: 'https://prep-22-jul-prep-3-portfolio.netlify.app/',
+    sourceCode: 'https://github.com/MLH-Fellowship/prep-portfolio-22.JUL.PREP.3',
+  },
 ];
 
 const modalDetails = document.createElement('div');
